8d1f78c9001f16c514220f8fe2fb13ee8f5f22e9
iamchandrakanth <ccmchandrakanth5@gmail.com>
2021-06-05 18:00:56 +0530

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    "./public/**/*.html",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './public/**/*.html',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
