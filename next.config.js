f735090659b926e418fc8c0fbde890888c973d35
Chandrakanth <ccmchandrakanth5@gmail.com>
2022-03-24 10:58:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         webpack5: true,
       },
     images: {
-        domains: ["links.papareact.com", "fakestoreapi.com", "www.google.com"]
+        domains: ["links.papareact.com", "fakestoreapi.com", "www.google.com", "chandrakanth.in"]
     },
     env: {
         stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`
